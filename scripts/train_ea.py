d652865c980f6e3851af1494517cb43faf8f40b9
Andrew Albright <andrewstevenalbright@gmail.com>
2022-08-30 15:25:33 -0700

    added bolt artifacts path

--- scripts/train_ea.py
+++ scripts/train_ea.py
@@ -4,10 +4,15 @@ import pprint
 from optimal_agents.utils.loader import Parameters
 from optimal_agents.utils.trainer import train_ea
 
+import turibolt as bolt
+import os
+
+data_path = os.path.join(bolt.ARTIFACT_DIR, "data")
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", "-p", type=str, required=True, help="Config file for experiment")
 parser.add_argument("--output", "-o", type=str, required=False, default=None)
 args = parser.parse_args()
 
 params = Parameters.load(args.path)
-train_ea(params, path=args.output)
+train_ea(params, path=data_path)
